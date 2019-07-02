f37a7ea845a5a6c66ee75de31d048ef959a491d2
obsol <33932119+read-0nly@users.noreply.github.com>
2019-07-02 19:22:22 -0400

    Create GatherForwardAndRedirectRules.ps1

---
+++ EXO/GatherForwardAndRedirectRules.ps1
@@ -0,0 +1,8 @@
+﻿foreach ($a in (Get-Mailbox -ResultSize Unlimited |select PrimarySMTPAddress)) {
+  Get-InboxRule -Mailbox $a.PrimarySMTPAddress | ?{
+    ($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectTo -ne $null)
+  } |
+  select MailboxOwnerID,Name,Identity,ForwardTo,ForwardAsAttachmentTo, RedirectTo, DeleteMessage | 
+  Export-Csv "C:\InboxRules.csv" -append 
+};
+echo "`a"
