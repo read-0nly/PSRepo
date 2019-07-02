41e61fe1eb977e626397eb973154931a74548cc8
obsol <33932119+read-0nly@users.noreply.github.com>
2019-07-02 19:05:38 -0400

    Create SetWallpaper.ps1

---
+++ Utility/SetWallpaper.ps1
@@ -0,0 +1,53 @@
+﻿#Set the style and image URL
+[string]$style = "Fill"
+[string]$url = "http://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE2r0Th?ver=5b7d"
+
+#Hashtable of values for setting wallpaper styles
+$WallpaperStyleValues = @{
+	"Fill" = @(10,0);
+	"Fit" = @(6,0);
+	"Stretch" = @(2,0);
+	"Span" = @(22,0);
+	"Tile" = @(0,1);
+	"Center" = @(0,0)
+}
+#Generate local file path for image
+$localFilePath = [system.environment]::GetEnvironmentVariable("localappdata") + "\background.jpg"
+#Download image to local path
+Start-BitsTransfer $url -Destination $localFilePath
+#Delay for propagation - This may still not be enough. If the wallpaper isn't updating, try doubling it. This is likely because of the bits transfer
+Start-Sleep -s 15
+#set the new wallpaper, pipe the confirmation to null because any console interaction will cause the script to fail running as a deployed script
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $localFilePath /f > $null
+#Set the wallpaper style details
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d $WallpaperStyleValues[$style][0] /f > $null
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d $WallpaperStyleValues[$style][1] /f > $null
+#Create minimalist User32 interface class
+try {
+	Add-Type @"
+		using System;
+		using System.Runtime.InteropServices;
+		using Microsoft.Win32;
+		namespace Wallpaper
+		{
+
+			public class Setter
+			{
+				[DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+				private static extern int SystemParametersInfo ( int uAction, int uParam, string lpvParam, int fuWinIni );
+				public static void SetWallpaper ( string path )
+				{
+					SystemParametersInfo( 20, 0, path, 0x1 | 0x2 );
+				}
+			}
+		}
+"@
+} catch {}
+
+#Set wallpaper with API
+[Wallpaper.Setter]::SetWallpaper($localFilePath)
+# sometimes the wallpaper only changes after the second run, so I'll run it twice!
+sleep -ms 200
+[Wallpaper.Setter]::SetWallpaper($localFilePath)
+#Refresh wallpaper
+rundll32.exe user32.dll, UpdatePerUserSystemParameters > $null
