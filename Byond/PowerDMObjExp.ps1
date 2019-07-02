82d3652ddf6c493ef72198fd02a138f33ff48c0a
obsol <33932119+read-0nly@users.noreply.github.com>
2019-07-01 21:57:15 -0400

    Create PowerDMObjExp.ps1

---
+++ Byond/PowerDMObjExp.ps1
@@ -0,0 +1,12 @@
+﻿$nakedScript = cat "human.dm"
+$DefinitionStarts = new-object System.Collections.HashTable 
+#Pull the start of every code block
+for($i = 0;$i -lt $nakedScript.count;$i++){
+    if($nakedScript[$i].StartsWith("/") -and -not $nakedScript[$i].StartsWith("//")){
+        $DefinitionStarts.add($nakedScript[$i],$i)
+    }
+}
+#Pull Functions from Classes
+$Classes = new-object System.Collections.HashTable 
+$Functions = new-object System.Collections.HashTable 
+$DefinitionStarts.Keys|%{if($_ -match "\(.*\)"){$Functions.add($_,$DefinitionStarts[$_])}else{$Classes.add($_,$DefinitionStarts[$_])}}
