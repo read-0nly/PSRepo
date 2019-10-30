af096aef4be15ff0a6f4e501b4017ac4f199d5b2
obsol <33932119+read-0nly@users.noreply.github.com>
2019-10-30 00:27:41 -0400

    Update EVTXRipper.ps1

--- Utility/EVTXRipper.ps1
+++ Utility/EVTXRipper.ps1
@@ -1,7 +1,7 @@
 param(
     $FolderPath
 )
-$workDir = $FolderPath
+$script:workDir = $FolderPath
 $EvtLvlColors = @{
     "Information" = "White";
     "Verbose" = "Cyan";
@@ -13,25 +13,25 @@ $EvtLvlColors = @{
 
 function introFetch(){
     introwatermark(-1) 
-    $workDir  =  (read-host "Please enter the path to the directory containing mdmdiagnosticstool output ('.' is the alias of the current directory)")
+    $script:workDir  =  (read-host "Please enter the path to the directory containing mdmdiagnosticstool output ('.' is the alias of the current directory)")
 }
 
 function introWatermark($i){
     $wmLine  = "------------------------------------------------"
-    $wmTitle = "------------:   EVTXRipper  v0.1:   ------------"
+    $wmTitle = "-------:        EVTXRipper  v0.1:        -------"
     $wmState = @(
                "-------:   FETCHING WORKING DIRECTORY:   -------",
-               "--------:   INITIALIZING EVENT ARRAY:   --------",
-               "-----------:   GROUPING  EVENTIDS:   -----------",
-               "-----------:   OUTPUTING  RESULTS:   -----------",
-               "------------:   SAVING IS CARING:   ------------"
+               "-------:    INITIALIZING EVENT ARRAY:    -------",
+               "-------:       GROUPING  EVENTIDS:       -------",
+               "-------:       OUTPUTING  RESULTS:       -------",
+               "-------:        SAVING IS CARING:        -------"
                )
-    $wmColor =@("Red","Yellow","Yellow","Yellow", "Green")
+    $wmColor =@("Red","Yellow","Green","Cyan", "Magenta")
     cls
     start-sleep -Milliseconds 5
     write-host $wmLine -ForegroundColor white
     $splitState = $wmTitle.split(":")
-    $curColor = "RED"
+    $curColor = $wmColor[$i+1]
     $Middle = (" "+$splitState[1]+" ")
     write-host $splitState[0] -ForegroundColor white -NoNewline
     start-sleep -Milliseconds 5
@@ -62,9 +62,11 @@ function runEvtxRipper(){
     introWatermark(2)
     $AllUniques = $AllGrouped | %{($_.group | sort-object timecreated)[0]}
     $Selection = @("level", "leveldisplayname", "timecreated", "message", "id", "logname", "processid", "machinename", "userid", "containerlog")
-    $AllUniques | sort-object timecreated | select-object -property $selection | out-gridview
+    $AllUniques | sort-object timecreated | select-object -property $selection | out-gridview -title "All Unique EventIDs - First Occurence"
     introWatermark(3)
     $AllEventsPath = $null
+    write-host
+    write-host
     if((read-host "Enter 'Y' to export All Events to csv").ToUpper() -eq "Y"){
         $AllEventsPath = (read-host "Enter path to directory to save file ('.' is the alias of the current directory)")
         #While path is invalid
@@ -81,6 +83,7 @@ function runEvtxRipper(){
     else{
         introwatermark(3)
         write-host "You have chosen not to export All Events to a file" -ForegroundColor red
+        write-host
     }
     $AllEventsPath = $null
     if((read-host "Enter 'Y' to export All Unique Events to csv").ToUpper() -eq "Y"){
@@ -103,10 +106,10 @@ function runEvtxRipper(){
     }
 }
 
-while(($workDir -eq $null)){
+while(($script:workDir -eq $null)){
     introfetch    
-    if(-not ($workDir -eq $null)){
-        while (-not (test-path $workDir) -or (-not ((dir $workDir+'\*.evtx').count -gt 0))){
+    if(-not ($script:workDir -eq $null)){
+        while (-not (test-path $script:workDir) -or (-not ((dir ($script:workDir+'\*.evtx')).count -gt 0))){
             introFetch
         }
     }
