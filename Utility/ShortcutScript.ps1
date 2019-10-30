4919572467e6e6e9d18e6108084b873f910a3490
obsol <33932119+read-0nly@users.noreply.github.com>
2019-10-30 18:58:49 -0400

    Create ShortcutScript.ps1

---
+++ Utility/ShortcutScript.ps1
@@ -0,0 +1,2 @@
+#This will effectively be a menu that calls scripts straight from github with iex (iwr ).content
+#Meant to simplify calling these things in general - run iex(iwr THISSCRIPT).content then run through the menu and it calls the rest
