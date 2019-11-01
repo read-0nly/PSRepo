ff199628fe26709088a05cbc07f564f06d070259
obsol <33932119+read-0nly@users.noreply.github.com>
2019-11-01 15:24:32 -0400

    Update ShortcutScript.ps1

--- Utility/ShortcutScript.ps1
+++ Utility/ShortcutScript.ps1
@@ -1,2 +1,7 @@
 #This will effectively be a menu that calls scripts straight from github with iex (iwr ).content
 #Meant to simplify calling these things in general - run iex(iwr THISSCRIPT).content then run through the menu and it calls the rest
+
+write-host "1 : EVTX Ripper"
+if((read-host "Please pick a script to run")-eq '1'){
+  iex (iwr "https://raw.githubusercontent.com/read-0nly/PSRepo/master/Utility/EVTXRipper.ps1").content
+}
