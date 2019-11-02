4214d62130c3357571d2f5040eb3a43bc24185ad
obsol <33932119+read-0nly@users.noreply.github.com>
2019-11-02 19:23:52 -0400

    Update ShortcutScript.ps1

--- Utility/ShortcutScript.ps1
+++ Utility/ShortcutScript.ps1
@@ -1,7 +1,7 @@
 #This will effectively be a menu that calls scripts straight from github with iex (iwr ).content
 #Meant to simplify calling these things in general - run iex(iwr THISSCRIPT).content then run through the menu and it calls the rest
 
-write-host "1 : EVTX Ripper"
+write-host "1: EVTX Ripper"
 write-host "2: InterTransmission"
 write-host "2: CompareForDupes"
 write-host "4: BatchJPG"
