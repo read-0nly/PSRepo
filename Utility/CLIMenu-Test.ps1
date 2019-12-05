439c1aa8d079a552fb869757a7f4b611d2d7a941
obsol <33932119+read-0nly@users.noreply.github.com>
2019-12-05 18:15:31 -0500

    Create CLIMenu-Test.ps1

---
+++ Utility/CLIMenu-Test.ps1
@@ -0,0 +1,135 @@
+$global:menu = [pscustomobject]@{
+    "MenuState" = 1
+    "Settings" = @{
+        "Vertical" = $false;
+        "Width"=30;
+        "Spacer" = "|";
+        "SelectionRange" = @(-1,0,1);
+        "SelectionColors" = @("DarkRed","DarkGray","DarkGreen")
+    };
+    "Cursor" = 1;
+    "Items" = @(
+        [pscustomobject]@{
+            "Name" = "Entry1";
+            "Command" = [scriptBlock]{echo "Entry 1"};
+            "Selected" = 0
+            "Selectable" = 0
+        },    
+        [pscustomobject]@{
+            "Name" = "Entry2";
+            "Command" = [scriptBlock]{echo "Entry 2"};
+            "Selected" = 0
+            "Selectable" = 0
+        },    
+        [pscustomobject]@{
+            "Name" = "Entry3";
+            "Command" = [scriptBlock]{echo "Entry 3"};
+            "Selected" = 0
+            "Selectable" = 1
+        }
+    )
+}
+
+$currentMenu = [ref]$global:Menu
+
+function padString($entry, $width){   
+    $string = ""   
+    $string = (&{if($entry.Name -eq $Menu.Items[$menu.Cursor].name){"> "}else{""}})+$entry.name+(&{if($entry.Name -eq $Menu.Items[$menu.Cursor].name){" <"}else{""}})
+    $pad = $menu.Settings["Width"] - $string.length
+    $string = $string.PadLeft([int]($pad/2)+$string.length)
+    $string = $string.PadRight($width)
+    return $string
+}
+
+function paintMenu($curMenu){
+    if($curMenu.Settings["Vertical"]){
+        $curMenu.Items | %{ 
+            write-host $curMenu.Settings["Spacer"] -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof(0)] -nonewline           
+            write-host ((padString $_ $curMenu.Settings["Width"])) -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof($_.selected)] -nonewline
+            write-host $curMenu.Settings["Spacer"] -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof(0)]
+        }
+    }
+    else{
+        write-host $curMenu.Settings["Spacer"] -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof(0)] -nonewline    
+        $curMenu.Items | %{
+            write-host ((padString $_ $curMenu.Settings["Width"])) -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof($_.selected)] -NoNewline
+            write-host $curMenu.Settings["Spacer"] -backgroundcolor $curMenu.settings["SelectionColors"][$curMenu.settings["SelectionRange"].indexof(0)] -NoNewline
+        }
+        write-host "";
+    }
+    write-host "";
+}
+
+function paintScreen(){
+    cls;
+    start-sleep -milliseconds 100;
+    
+    paintMenu $currentMenu.Value
+
+}
+
+function menuStep(){
+    if($psISE -eq $null){
+        switch -wildcard ([system.console]::readkey().Key){
+            "UpArrow" {
+                if($menu.cursor -gt 0){
+                    $Menu.Cursor--
+                }
+            }
+            "LeftArrow" {
+                if($menu.cursor -gt 0){
+                    $menu.Cursor--
+                }
+            }
+            "RightArrow" {
+                if($menu.cursor -lt $menu.Items.Count-1){
+                    $Menu.Cursor++
+                }
+            }
+            "DownArrow" {
+            if($menu.cursor -lt $menu.Items.Count-1){$Menu.Cursor++}}
+            "Spacebar" {
+                if(($global:Menu.Items[$global:Menu.Cursor].Selected -eq 0) -and ($global:Menu.Items[$global:Menu.Cursor].Selectable -gt 0)){
+                    $global:Menu.Items[$global:Menu.Cursor].Selected = 1
+                }else{
+                    if($global:Menu.Items[$global:Menu.Cursor].Selected -gt 0 -and ($global:Menu.Items[$global:Menu.Cursor].Selectable -gt 0)){
+                        $global:Menu.Items[$global:Menu.Cursor].Selected = -1
+                    }else{
+                        $global:Menu.Items[$global:Menu.Cursor].Selected = 0
+                    }
+                }
+            }
+            "*"{
+                paintScreen;
+            }
+            "Enter" {
+                & $global:Menu.Items[$global:Menu.Cursor].Command
+            }
+        }
+    }
+    else{
+        switch -wildcard (read-host "Enter + or - to move the cursor, Space to switch selection status of an item, = to run the menu item"){
+            "+"{if($global:menu.cursor -lt $global:menu.Items.Count-1){$global:Menu.Cursor++}}
+            "-"{if($global:menu.cursor -gt 0){$global:Menu.Cursor--}}
+            " "{
+                if(($global:Menu.Items[$global:Menu.Cursor].Selected -eq 0) -and ($global:Menu.Items[$global:Menu.Cursor].Selectable -gt 0)){
+                    $global:Menu.Items[$global:Menu.Cursor].Selected = 1
+                }else{
+                    if($global:Menu.Items[$global:Menu.Cursor].Selected -gt 0 -and ($global:Menu.Items[$global:Menu.Cursor].Selectable -gt 0)){
+                        $global:Menu.Items[$global:Menu.Cursor].Selected = -1
+                    }else{
+                        $global:Menu.Items[$global:Menu.Cursor].Selected = 0
+                    }
+                }
+            }
+            "*"{paintScreen}
+            "="{& $global:Menu.Items[$global:Menu.Cursor].Command;write-host "";}       
+        }
+    }
+}
+
+paintScreen
+
+while($true){
+    menuStep
+}
