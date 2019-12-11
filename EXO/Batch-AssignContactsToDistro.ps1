e73293559d8e1b6ae9bc7bb13e086f048a6e725f
obsol <33932119+read-0nly@users.noreply.github.com>
2019-12-11 16:41:11 -0500

    Update Batch-AssignContactsToDistro.ps1

--- EXO/Batch-AssignContactsToDistro.ps1
+++ EXO/Batch-AssignContactsToDistro.ps1
@@ -1,3 +1,11 @@
+#The CSV file technically only needs one column called name, with the display names of the contacts
+#Example:
+<#
+"Name"
+"Contact 1"
+"Contact 2"
+"Contact 3"
+#>
 $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
 Import-PSSession $ExchangeSession
 $ContactCSV = import-csv (read-host "Please enter the path to the Contact CSV file")
