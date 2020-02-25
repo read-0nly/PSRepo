2f4a156bcecacb2f3aa7c531cbbae0038196e8f2
obsol <33932119+read-0nly@users.noreply.github.com>
2020-02-25 16:34:26 -0500

    Update AutoPad-Base64.ps1

--- Utility/AutoPad-Base64.ps1
+++ Utility/AutoPad-Base64.ps1
@@ -1,1 +1,18 @@
-
+$hash = ""
+$while = $true
+while ($while){
+    try{
+        [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($hash)) >> $null
+        $while = $false
+        }
+    catch{
+        if(($hash.ToCharArray()[-1]-eq '=') -and ($hash.ToCharArray()[-2]-eq '=')){
+            $hash = $hash.replace("=","")
+            $hash= ($hash+"A")
+        }
+        else{
+            $hash= ($hash+"=")
+        }
+    }
+}
+$hash
