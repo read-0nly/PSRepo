2c74d35ee3bbe1bd557153899dfaafb0cc029d1f
obsol <33932119+read-0nly@users.noreply.github.com>
2020-04-02 19:05:26 -0400

    Create Get-DetectionPath.ps1

---
+++ MDMTools/Get-DetectionPath.ps1
@@ -0,0 +1,27 @@
+#Gets the detection paths of all win32 apps in intune
+install-module azuread
+$Workfolder = ([system.environment]::getfolderpath("Desktop")+"\AppControl")
+$IntuneSamplets = $Workfolder + "\IntuneSamples"
+
+if(-not(test-path $Workfolder)){
+mkdir $Workfolder
+}
+if(-not(test-path $IntuneSamplets)){
+mkdir $IntuneSamplets
+}
+
+$Tenant = (iex (iwr "https://raw.githubusercontent.com/microsoftgraph/powershell-intune-samples/master/CompanyPortalBranding/CompanyPortal_Get.ps1" -usebasicparsing).content)
+
+write-host "Connected to : " -ForegroundColor Green -NoNewline
+write-host ($Tenant.displayName) -ForegroundColor Magenta
+
+
+$uri = "https://graph.microsoft.com/beta/deviceappmanagement/mobileapps?select=id,displayname"
+$allApps = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).value
+$win32apps = @()
+
+$allapps | where-object {$_.'@odata.type' -like "*win32*"} | %{
+    $uri = ("https://graph.microsoft.com/beta/deviceappmanagement/mobileapps/"+$_.id)
+    $win32apps +=@((Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get))
+}
+$win32Apps | select displayname,detectionrules | %{[pscustomobject]@{"Name"=$_.displayname;"Path"=($_.detectionRules.path + $_.detectionRules.fileorfoldername)}}
