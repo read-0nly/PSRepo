8d9e126e4b4b2a507e44bfe4533c91994b7ed4f2
obsol <33932119+read-0nly@users.noreply.github.com>
2020-04-02 20:01:30 -0400

    Create Generate-AppcontrolExcusions.ps1

---
+++ Generate-AppcontrolExcusions.ps1
@@ -0,0 +1,37 @@
+install-module azuread
+$Workfolder = ([system.environment]::getfolderpath("Desktop")+"\AppControl")
+$xmlPath = ($WorkFolder+"\Output.xml")
+$binPath = ($WorkFolder+"\Output.bin")
+
+$Tenant = (iex (iwr "https://raw.githubusercontent.com/microsoftgraph/powershell-intune-samples/master/CompanyPortalBranding/CompanyPortal_Get.ps1" -usebasicparsing).content)
+
+write-host "Connected to : " -ForegroundColor Green -NoNewline
+write-host ($Tenant.displayName) -ForegroundColor Magenta
+
+
+$uri = "https://graph.microsoft.com/beta/deviceappmanagement/mobileapps?select=id,displayname"
+$allApps = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).value
+$win32apps = @()
+
+$allapps | where-object {$_.'@odata.type' -like "*win32*"} | %{
+    $uri = ("https://graph.microsoft.com/beta/deviceappmanagement/mobileapps/"+$_.id)
+    $win32apps +=@((Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get))
+}
+$PathTable = $win32Apps | select displayname,detectionrules | %{[pscustomobject]@{"Name"=$_.displayname;"Path"=($_.detectionRules.path + $_.detectionRules.fileorfoldername)}}
+
+$PathTable | %{
+    $_.Path
+
+$Rule = New-CIPolicyRule -FilePathRule $_.path
+if(test-path $xmlPath){
+    Merge-CIPolicy -outputfile $xmlPath -PolicyPaths $xmlPath -Rules $Rule
+}
+else{
+    New-CIPolicy -FilePath $xmlPath -Rules $Rule
+}
+    
+}
+Set-CIPolicyIdInfo -FilePath $xmlPath -reset
+ConvertFrom-CIPolicy -XmlFilePath $xmlPath -BinaryFilePath $binPath
+#$xmlPath
+#$binPath
