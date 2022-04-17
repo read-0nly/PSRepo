5f69cc0cd15a90331a15187919dd7b6c39f071ad
obsol <33932119+read-0nly@users.noreply.github.com>
2022-04-17 01:12:29 -0400

    Create remote-controlpanel.ps1

---
+++ Utility/remote-controlpanel.ps1
@@ -0,0 +1,160 @@
+#[System.Net.HttpListener]::IsSupported
+param(
+[string[]]$URIs = @("http://localhost","http://127.0.0.1"),
+[string[]]$Ports=@("80")
+)
+$URISets = $URIs | %{$uri = $_;$Ports | %{echo ($uri.ToString()+":"+$_.ToString()+"/")}}
+$listener = new-object System.Net.HttpListener
+$URISets | %{$listener.Prefixes.Add($_)}
+$listener.Start()
+$global:stopLoop = $false;
+$global:defaultCommands = [PSCustomObject]@{
+		"Default"=[PSCustomObject]@{
+			"1_SaveMenu"=  "convertto-json `$global:commands | out-file ./actionMenu.json"
+			"2_ReloadMenu"=  "`$global:commands = convertfrom-json (cat ./actionMenu.json -raw)"
+			"3_ResetMenu"=  "`$global:commands = `$global:defaultCommands"
+	}
+}
+$global:commands = $global:defaultCommands
+if(test-path ./actionMenu.json) {$global:commands = convertfrom-json (cat ./actionMenu.json -raw)}
+while(-not $global:stopLoop){
+	$context = $listener.GetContext();
+	$req = $context.Request
+	$rawUrl = $req.RawUrl
+	$Parameters = @{}
+	$rawUrl = $rawUrl.Split("?")
+	$Path = $rawUrl[0]
+	$rawParameters = $rawUrl[1]
+	if ($rawParameters) {
+		$rawParameters = $rawParameters.Split("&")
+		foreach ($rawParameter in $rawParameters) {
+			$Parameter = $rawParameter.Split("=")
+			$Parameters.Add($Parameter[0], $Parameter[1])
+		}
+	}
+	$resp = $context.Response
+	if($parameters["stop"] -eq "stop"){
+		$stopLoop = $true
+		$responseStr="<h1>STOPPING</h1>"
+	}else{
+		if($parameters["panel"] -eq $null){
+			$parameters["panel"] = "Default"
+		}else{			
+			if($parameters["cmd"] -ne $null){
+				if($global:commands.($parameters["panel"]).($parameters["cmd"]) -ne $null){
+					write-host "----"
+					$global:commands.($parameters["panel"]).($parameters["cmd"])
+					write-host "----"
+					iex $global:commands.($parameters["panel"]).($parameters["cmd"])
+				}
+			}
+		}
+		if($true){ #Stashing the response text because big
+		[string]$responseStr=@"
+			<html>
+				<head>
+					<style type="text/css">
+					body{
+						background-color:#1C1B22
+					}
+					td{
+						border:1px solid white;
+						margin-left:auto;
+						margin-right:auto;
+						color:white;
+						width:1%;
+						font-size:16pt;
+					}
+					td:hover{
+						border:3px solid #553377;
+						background-color:#33224f;
+						margin-left:auto;
+						margin-right:auto;
+						color:#AA77FF;
+					}
+					.lastClicked{
+						border:3px solid #00ff00;
+						background-color:#004f00;
+						border:1px solid white;
+						margin-left:auto;
+						margin-right:auto;
+						color:#22FF22;
+					}
+					table{
+						text-align:center;
+						font-family:sans-serif;
+						border-collapse:collapse;
+						width:100%;
+						height:15%;
+					}
+					.tablehead{
+						background-color:#33224f;
+						color:#AA77FF;
+						font-size:20pt;
+						height:28px;
+					}
+					.buttonTable{
+						background-color:#33224f;
+						color:#AA77FF;
+						font-size:20pt;
+						height:84%;
+					}
+					</style>
+				</head>
+				<body>
+
+					<table>
+						<tbody>
+							<tr>
+								<td colspan="12" class="tablehead">Menu</td>
+							</tr>
+							<tr>
+"@
+		($global:commands.psobject.properties.name) | %{
+			$wasclicked =""
+			if($_ -eq $parameters["panel"]){$wasclicked=echo " class=`"lastClicked`""}
+			$responseStr+="<td"+$wasclicked+" onClick=`"window.location=window.location.toString().split('?')[0]+'?panel=$_'`">" +
+				$_ + "</td>"
+			
+		}
+		$responseStr+=@"
+							</tr>
+						</tbody>
+					</table>
+
+					<table class="buttontable">
+						<tbody>
+							<tr>
+								<td class="tablehead" colspan="12">Buttons</td>
+							</tr>		
+							<tr>
+"@
+		$i = 0
+		($global:commands.($parameters["panel"]).psobject.properties.name | sort-object) | %{
+			$i++
+			$wasclicked =""
+			if($_ -eq $parameters["cmd"]){$wasclicked=echo " class=`"lastClicked`""}
+			$responseStr+="<td"+$wasclicked+" onClick=`"window.location=window.location.toString().split('?')[0]+'?cmd="+$_+"&panel="+$parameters["panel"]+"'`">" +
+				$_ + "</td>"
+				if(($i%4) -eq 0){$responseStr+="</tr><tr>"}
+			
+		}
+
+		$responseStr+=@"
+</tr>
+						</tbody>
+					</table>
+
+				</body>
+			</html>
+"@		
+		$responseStr=$responseStr.Replace("<tr></tr>","")
+	}
+	}
+	$responseBytes= [System.Text.Encoding]::UTF8.GetBytes($responseStr);
+	$resp.ContentLength64=$responseBytes.Length
+	$output=$resp.OutputStream
+	$output.Write($responseBytes,0,$responseBytes.Length)
+	$output.Close()
+}
+$listener.Stop();
